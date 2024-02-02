ea98e7eb40f0b354c6257d969c3553ed20872994
AAAAAAAAHHH <151954437+AAAAAAAAHHH@users.noreply.github.com>
2024-02-02 11:30:01 +0100

    Add files via upload

---
+++ zene proj/zene proj/Program.cs
@@ -0,0 +1,23 @@
+﻿using proj;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zene_proj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            menuControl menu = new menuControl();
+
+            menu.Menu();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}

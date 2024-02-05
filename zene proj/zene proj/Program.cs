86a806468a36db2ed8b8d8016989164fdda2c341
AAAAAAAAHHH <151954437+AAAAAAAAHHH@users.noreply.github.com>
2024-02-05 10:14:52 +0100

    Add files via upload

---
+++ zene proj/zene proj/Program.cs
@@ -0,0 +1,26 @@
+﻿using proj;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zene_proj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            menuControl menu = new menuControl();
+
+            int menuIndex = menu.Menu();
+
+            Console.Clear();
+
+            Console.WriteLine($"\nKiválsztott menüpont: {menuIndex+1}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ zene proj/zene proj/menuControl.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proj
+{
+    internal class menuControl
+    {
+        public int Menu()
+        {
+            // A menüpontok neve
+            string[] menu =
+            {
+                "Menüpont 1",
+                "Menüpont 2",
+                "Menüpont 3",
+                "Menüpont 4",
+            };
+
+            // Ez a váltzó tárolja a lenyomott billentyű adatait
+            ConsoleKeyInfo keyInfo;
+
+            // A jelenlegi menüpont indexe
+            int currentMenu = 0;
+
+            // Eltünteti a kurzort
+            Console.CursorVisible = false;
+
+            do
+            {
+                // A konozol törlése, hogy frissíteni tudja
+                Console.Clear();
+
+                Console.WriteLine();
+
+                // A menüpontok kiíratása
+                for (int i = 0; i < menu.Length; i++)
+                {
+                    // Ha a kiírandó menüpont jelenleg ki van választva, akkor átszínezi pirosra
+                    if (currentMenu == i)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(menu[i]);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Console.WriteLine(menu[i]);
+                    }
+                }
+
+                // Nyíl inputra vár
+                keyInfo = Console.ReadKey();
+
+                // Megnézi, hogy melyik billentyűt nyomtuk le
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                    {
+                        // Ha van lefelé hely akkor lefelé megy
+                        if (currentMenu == 0)
+                        {
+                            currentMenu = menu.Length - 1;
+                        }
+                        // Ha nincs, akkor visszaugrik a menü elejére
+                        else
+                        {
+                            currentMenu -= 1;
+                        }
+
+                        break;
+                    }
+
+                    case ConsoleKey.DownArrow:
+                    {
+                        // Ha van felfelé hely akkor felfelé megy
+                        if (currentMenu != menu.Length - 1)
+                        {
+                            currentMenu += 1;
+                        }
+                        // Ha nincs, akkor visszaugrik a menü aljára
+                        else
+                        {
+                            currentMenu = 0;
+                        }
+                        break;
+                    }
+                }
+
+            } while (keyInfo.Key != ConsoleKey.Enter);
+
+            return currentMenu;
+        }
+    }
+}
